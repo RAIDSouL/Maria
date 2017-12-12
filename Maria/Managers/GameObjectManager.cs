1dc14bd7726835f1ba61389ef16b4d3367c49ca6
nitpum <nitpum@outlook.com>
2017-12-13 00:37:25 +0700

    Add GameObject manager

---
+++ Maria/Managers/GameObjectManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maria
+{
+    public class GameObjectManager
+    {
+        public static List<GameObject> list = new List<GameObject>();
+
+        public static void Add (GameObject gameObject)
+        {
+            list.Add(gameObject);
+        }
+    }
+}
