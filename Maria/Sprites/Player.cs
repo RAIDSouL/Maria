7c3066320bee8b159375fab47af7d64cbaf3b86b
nitpum <nitpum@outlook.com>
2017-12-13 12:54:21 +0700

    Rename variable

--- Maria/Sprites/Player.cs
+++ Maria/Sprites/Player.cs
@@ -12,6 +12,14 @@ namespace Maria.Sprites
 {
     public class Player : Sprite
     {
+        public float Speed = 0.1f;
+
+        public Player(Dictionary<string, Animation> _animation, float _gravity)
+           : base(_animation, _gravity)
+        {
+
+        }
+
         public Player(Texture2D texture)
             : base(texture)
         {
@@ -20,17 +28,29 @@ namespace Maria.Sprites
 
         public override void Update(GameTime gameTime, List<Sprite> sprites)
         {
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                Velocity.X += 0.1f;
+                Console.WriteLine("Press");
+            }
+            else if (Keyboard.GetState().IsKeyUp(Keys.Right))
+                Velocity.X = 0;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) Velocity.X -= Speed;
+            else Velocity.X = 0;
 
             foreach (var sprite in sprites)
             {
                 if (sprite == this)
                     continue;
                 //collide with box
+                /*
                 if (this.Velocity.X > 0 && this.IsTouchingLeft(sprite) || this.Velocity.X > 0 && this.IsTouchingRight(sprite))
                     this.Velocity.X = 0;
 
                 if (this.Velocity.Y > 0 && this.IsTouchingTop(sprite) || this.Velocity.X > 0 && this.IsTouchingRight(sprite))
                     this.Velocity.Y = 0;
+                    */
             }
         }
     }
