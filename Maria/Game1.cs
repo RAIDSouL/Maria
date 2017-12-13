860011b520b5477a705b26b16a45e1bfd80fb9fe
Windows 10 <Windows 10@DESKTOP-VEMFB4G>
2017-12-13 16:10:27 +0700

    test nam

--- Maria/Game1.cs
+++ Maria/Game1.cs
@@ -63,6 +63,8 @@ namespace Maria
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
         /// </summary>
+        /// 
+
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
