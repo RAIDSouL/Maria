884ea355d4408fdaa67e7e3fca16ce4a0e7b4a71
nitpum <nitpum@outlook.com>
2017-12-14 23:49:48 +0700

    Add Block

---
+++ Maria/Sprites/Block.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maria.Sprites
+{
+    public class Block : Sprite
+    {
+        public Block(Texture2D texture) : base(texture)
+        {
+            _texture = texture;
+            gravity = 0;
+            physicsType = EPhysics.Static;
+        }
+    }
+}
