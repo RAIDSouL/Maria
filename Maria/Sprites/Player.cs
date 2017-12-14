0f6bfbd5868b6bdeed2f90f540ba6c73378db8af
RAIDsoul <Thunderock01@gmail.com>
2017-12-14 21:04:21 +0700

    FOR demacia

--- Maria/Sprites/Player.cs
+++ Maria/Sprites/Player.cs
@@ -60,13 +60,13 @@ namespace Maria.Sprites
                 if (sprite == this)
                     continue;
                 //collide with box
-
+                /*
                 if (this.Velocity.X > 0 && this.IsTouchingLeft(sprite) || this.Velocity.X > 0 && this.IsTouchingRight(sprite))
                     this.Velocity.X = 0;
 
                 if (this.Velocity.Y > 0 && this.IsTouchingTop(sprite) || this.Velocity.Y > 0 && this.IsTouchingBottom(sprite))
                     this.Velocity.Y = 0;
-                    
+                    */
             }
             Position += Velocity;
             Velocity = Vector2.Zero;
