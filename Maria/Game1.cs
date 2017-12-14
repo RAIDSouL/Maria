3ea79a2da7d95a3f47e42adec1f33f081cd3664f
nitpum <nitpum@outlook.com>
2017-12-14 20:28:38 +0700

    Reset back to song now loop

--- Maria/Game1.cs
+++ Maria/Game1.cs
@@ -97,6 +97,7 @@ namespace Maria
             player = spriteManager.List[0];
             // TODO: use this.Content to load your game content here
             LoadSfx("jump");
+            LoadSfx("hit");
 
             LoadMap("level1");
 
@@ -131,7 +132,7 @@ namespace Maria
         {
             soundeffects.Add(Content.Load<SoundEffect>(Path.Combine("sfx/" + sfxName)));
         }
-    
+        
 
         /// <summary>
         /// Allows the game to run logic such as updating the world,
--- Maria/Sprites/Player.cs
+++ Maria/Sprites/Player.cs
@@ -44,11 +44,13 @@ namespace Maria.Sprites
             if (ishit)
             {
                 translation.X = -Speed;
+                Position = Vector2.Zero;
             }
 
             if (Keyboard.GetState().IsKeyDown(Input.Jump) && grounded)
             {
                 jumpForce = 1f;
+
             }
 
             Console.WriteLine(jumpForce);
